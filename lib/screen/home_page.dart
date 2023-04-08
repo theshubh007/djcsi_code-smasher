c7f612901c9422470d5da537de60f839d7dc119e
theshubh007 <thekingshubh07@gmail.com>
2023-04-08 21:57:50 +0530

    img

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,4 +1,5 @@
-import 'dart:ffi';
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -11,32 +12,76 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  File? img;
+  final ImagePicker _picker = ImagePicker();
+  
+  File? selectedImage;
+  String base64Image = "";
+
+  Future<void> chooseImage(type) async {
+    // ignore: prefer_typing_uninitialized_variables
+    var image;
+    if (type == "camera") {
+      image = await ImagePicker()
+          .pickImage(source: ImageSource.camera, imageQuality: 10);
+    } else {
+      image = await ImagePicker()
+          .pickImage(source: ImageSource.gallery, imageQuality: 25);
+    }
+    if (image != null) {
+      setState(() {
+        selectedImage = File(image.path);
+        base64Image = base64Encode(selectedImage!.readAsBytesSync());
+        // won't have any error now
+      });
+    }
+  }
+
   @override
-  final ImagePicker picker = ImagePicker();
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Color(0xff6096B4),
+      backgroundColor: const Color(0xff6096B4),
       body: SafeArea(
           child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            CircleAvatar(
+              radius: 60,
+              backgroundColor: Colors.red,
+              child: Padding(
+                padding: const EdgeInsets.all(8), // Border radius
+                child: ClipOval(
+                    child: selectedImage != null
+                        ? Image.file(
+                            selectedImage!,
+                            fit: BoxFit.cover,
+                            height: 100,
+                            width: 100,
+                          )
+                        : Image.network(
+                            'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
+                            fit: BoxFit.cover,
+                            height: 100,
+                            width: 100,
+                          )),
+              ),
+            ),
             InkWell(
               onTap: () async {
-                var status = await Permission.photos.status;
-                print(status);
-                await picker.pickImage(source: ImageSource.gallery);
+                chooseImage("galary");
               },
               child: Card(
                 color: Colors.transparent,
                 elevation: 10,
                 child: Container(
                     width: width * 0.80,
-                    padding: EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
                     decoration: BoxDecoration(
                         color: Colors.lightBlue.shade100,
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10)),
                         border: Border.all(color: Colors.white)),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.start,
@@ -46,10 +91,10 @@ class _HomePageState extends State<HomePage> {
                           height: 80,
                           color: Colors.black,
                         ),
-                        SizedBox(
+                        const SizedBox(
                           width: 18,
                         ),
-                        Text(
+                        const Text(
                           "Upload Photo",
                           style: TextStyle(
                               fontSize: 16,
@@ -60,14 +105,14 @@ class _HomePageState extends State<HomePage> {
                     )),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 15,
             ),
             InkWell(
               onTap: () async {
-                var status = await Permission.photos.status;
-                print(status);
-                await picker.pickImage(source: ImageSource.gallery);
+                // var status = await Permission.photos.status;
+                // print(status);
+                // await picker.pickImage(source: ImageSource.gallery);
               },
               child: Card(
                 color: Colors.transparent,
@@ -75,19 +120,20 @@ class _HomePageState extends State<HomePage> {
                 child: Container(
                     // alignment: Alignment.,
                     width: width * 0.80,
-                    padding: EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
                     decoration: BoxDecoration(
                         color: Colors.lightBlue.shade100,
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10)),
                         border: Border.all(color: Colors.white)),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         Image.asset("assets/aadharCard.png", height: 90),
-                        SizedBox(
+                        const SizedBox(
                           width: 9,
                         ),
-                        Text(
+                        const Text(
                           "Upload Aadharcard ",
                           style: TextStyle(fontSize: 16, fontFamily: "Anuphan"),
                         ),
@@ -95,14 +141,14 @@ class _HomePageState extends State<HomePage> {
                     )),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 15,
             ),
             InkWell(
               onTap: () async {
-                var status = await Permission.photos.status;
-                print(status);
-                await picker.pickImage(source: ImageSource.gallery);
+                // var status = await Permission.photos.status;
+                // print(status);
+                // await picker.pickImage(source: ImageSource.gallery);
               },
               child: Card(
                 color: Colors.transparent,
@@ -110,19 +156,20 @@ class _HomePageState extends State<HomePage> {
                 child: Container(
                     // alignment: Alignment.,
                     width: width * 0.80,
-                    padding: EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
                     decoration: BoxDecoration(
                         color: Colors.lightBlue.shade100,
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10)),
                         border: Border.all(color: Colors.white)),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         Image.asset("assets/panCard.png", height: 90),
-                        SizedBox(
+                        const SizedBox(
                           width: 10,
                         ),
-                        Text(
+                        const Text(
                           "Upload Pancard",
                           style: TextStyle(fontSize: 16, fontFamily: "Anuphan"),
                         ),
