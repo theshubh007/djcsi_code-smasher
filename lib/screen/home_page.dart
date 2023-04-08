4a49244590fc3b08ce854552ab8a6bcfb3d3c883
Savan Gangani <savangangani8518@gmail.com>
2023-04-08 21:53:18 +0530

    Add FormField

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,5 +1,7 @@
 import 'dart:ffi';
 import 'dart:io';
+import 'package:auth_employee/Firebase_Services.dart';
+import 'package:auth_employee/style/text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -13,7 +15,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final formKey = GlobalKey<FormState>();
-
+  bool formVisible = false;
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _adharController = TextEditingController();
   final TextEditingController _experienceController = TextEditingController();
@@ -152,9 +154,18 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ),
-                Padding(padding: EdgeInsets.only(top: 30, right: 50, left: 50),
+                Padding(
+                  padding: EdgeInsets.only(top: 30, right: 40, left: 40),
                 child: InkWell(
-                  onTap: (){},
+                  onTap: (){
+                    setState(() {
+                      if(!formVisible){
+                        formVisible=true;
+                      }else{
+                        formVisible=false;
+                      }
+                    });
+                  },
                   child: Container(
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.all(Radius.circular(10)),
@@ -166,7 +177,305 @@ class _HomePageState extends State<HomePage> {
                     child: Text("Verify", style: TextStyle(fontSize: 18, fontFamily: "Alkatra"),),
                   ),
                 ),
-                )
+                ),
+                Visibility(
+                  visible: formVisible,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 30, right: 30, left: 30),
+                    child: Form(
+                    key: formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Employee name",
+                                textAlign: TextAlign.left,
+                                style: formText
+                              ),
+                              const SizedBox(
+                                height: 2,
+                              ),
+                              TextFormField(
+                                controller: _nameController,
+                                decoration: const InputDecoration(
+                                  hintText: "Enter name of the employee",
+                                  border: OutlineInputBorder(),
+                                  focusColor: Colors.white10,
+                                  focusedBorder: OutlineInputBorder(
+                                    // borderRadius: BorderRadius.circular(25.0),
+                                    borderSide: BorderSide(
+                                      width: 0.8,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter some text';
+                                  }
+                                  return null;
+                                },
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Employee aadhar number",
+                                textAlign: TextAlign.left,
+                                style: formText
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              TextFormField(
+                                controller: _adharController,
+                                keyboardType: TextInputType.number,
+                                decoration: const InputDecoration(
+                                  hintText: "Enter adhar number of employee",
+                                  border: OutlineInputBorder(),
+                                  focusColor: Colors.white10,
+                                  focusedBorder: OutlineInputBorder(
+                                    // borderRadius: BorderRadius.circular(25.0),
+                                    borderSide: BorderSide(
+                                      width: 0.8,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter some text';
+                                  }
+                                  return null;
+                                },
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Employee PAN number",
+                                textAlign: TextAlign.left,
+                                style: formText
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              TextFormField(
+                                controller: _pancardController,
+                                decoration: const InputDecoration(
+                                  hintText: "Enter PAN number of employee",
+                                  border: OutlineInputBorder(),
+                                  focusColor: Colors.white10,
+                                  focusedBorder: OutlineInputBorder(
+                                    // borderRadius: BorderRadius.circular(25.0),
+                                    borderSide: BorderSide(
+                                      width: 0.8,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter some text';
+                                  }
+                                  return null;
+                                },
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Employee Year of experience",
+                                textAlign: TextAlign.left,
+                                style: formText
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              TextFormField(
+                                controller: _experienceController,
+                                keyboardType: TextInputType.number,
+                                decoration: const InputDecoration(
+                                  hintText: "Enter Year of experience of employee",
+                                  border: OutlineInputBorder(),
+                                  focusColor: Colors.white10,
+                                  focusedBorder: OutlineInputBorder(
+                                    // borderRadius: BorderRadius.circular(25.0),
+                                    borderSide: BorderSide(
+                                     color: Colors.white
+                                    ),
+                                  ),
+                                ),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter some text';
+                                  }
+                                  return null;
+                                },
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Employee Expertice",
+                                textAlign: TextAlign.left,
+                                style: formText
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              TextFormField(
+                                controller: _experticeController,
+                                decoration: const InputDecoration(
+                                  hintText: "Enter expertice of employee",
+                                  border: OutlineInputBorder(),
+                                  // focusColor: Colors.white10,
+                                  focusedBorder: OutlineInputBorder(
+                                    // borderRadius: BorderRadius.circular(25.0),
+                                    borderSide: BorderSide(
+                                      strokeAlign: 12,
+                                      width: 0.8,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter some text';
+                                  }
+                                  return null;
+                                },
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 30, right: 20, left: 20, bottom: 30),
+                          child: InkWell(
+                            onTap: () async {
+                              if (formKey.currentState!.validate()) {
+                                bool present = await Firebase_Serv
+                                    .check_employee_is_already_registered(
+                                    _adharController.text.trim(),
+                                    _pancardController.text.trim());
+
+                                if (present) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                          content: Text(
+                                              "Employee is already registered with this adhar number or pancard number")));
+                                } else {
+                                  await Firebase_Serv.upload_employee_data_bothplace(
+                                      _nameController.text.trim(),
+                                      _adharController.text.trim(),
+                                      _pancardController.text.trim(),
+                                      _experienceController.text.trim(),
+                                      _experticeController.text.trim());
+                                  setState(() {
+                                    clearcontroller();
+                                  });
+                                }
+                              }
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                                  border: Border.all(color: Colors.white)
+                              ),
+                              alignment: Alignment.center,
+                              height: 40,
+                              width: width,
+                              child: Text("Submit", style: TextStyle(fontSize: 18, fontFamily: "Alkatra"),),
+                            ),
+                          ),
+                        ),
+                        // InkWell(
+                        //   onTap: () async {
+                        //     await imgFromGallery();
+                        //   },
+                        //   child: Container(
+                        //     // alignment: Alignment.,
+                        //       width: width * 0.80,
+                        //       padding: const EdgeInsets.all(10),
+                        //       decoration: const BoxDecoration(
+                        //         color: Colors.black12,
+                        //         borderRadius: BorderRadius.all(Radius.circular(07)),
+                        //         // border: Border.all(color: Colors.red)
+                        //       ),
+                        //       child: Row(
+                        //         mainAxisAlignment: MainAxisAlignment.start,
+                        //         children: [
+                        //           Image.asset("assets/addPhoto.png", height: 85),
+                        //           const SizedBox(
+                        //             width: 18,
+                        //           ),
+                        //           const Text(
+                        //             "Add Photo",
+                        //             style: TextStyle(fontSize: 20),
+                        //           ),
+                        //         ],
+                        //       )),
+                        // ),
+                        // const SizedBox(
+                        //   height: 15,
+                        // ),
+                        // InkWell(
+                        //   onTap: () {
+                        //     print("OK");
+                        //   },
+                        //   child: Container(
+                        //       // alignment: Alignment.,
+                        //       width: width * 0.80,
+                        //       padding: const EdgeInsets.all(10),
+                        //       decoration: const BoxDecoration(
+                        //         color: Colors.black12,
+                        //         borderRadius: BorderRadius.all(Radius.circular(07)),
+                        //         // border: Border.all(color: Colors.red)
+                        //       ),
+                        //       child: Row(
+                        //         mainAxisAlignment: MainAxisAlignment.start,
+                        //         children: [
+                        //           Image.asset("assets/aadharCard.png", height: 90),
+                        //           const SizedBox(
+                        //             width: 9,
+                        //           ),
+                        //           const Text(
+                        //             "Add Aadharcard Photo",
+                        //             style: TextStyle(fontSize: 18),
+                        //           ),
+                        //         ],
+                        //       )),
+                        // ),
+
+                      ],
+                    ),
+                ),
+                  ),),
               ],
             ),
           ),
@@ -175,3 +484,9 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+
+
+
+
+
